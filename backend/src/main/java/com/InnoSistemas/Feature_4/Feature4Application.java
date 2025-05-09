5455b283c288d99b65e90307d2a7a9c6275b17a1
jhongarciap <jhon.garciap@udea.edu.co>
2025-05-09 10:31:52 -0500

    inicialización

---
+++ backend/src/main/java/com/InnoSistemas/Feature_4/Feature4Application.java
@@ -0,0 +1,13 @@
+package com.InnoSistemas.Feature_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Feature4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Feature4Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/InnoSistemas/Feature_4/Feature4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InnoSistemas.Feature_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Feature4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
